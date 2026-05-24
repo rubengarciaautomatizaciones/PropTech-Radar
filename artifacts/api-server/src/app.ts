b4d5a9f232168f4fec8c799a0f24dba91559e8d9
AIMAN ENNACIRI BARTAT <ruben.automatizaciones@gmail.com>
2026-05-24 19:18:04 +0000

    f

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,38 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
+import { IncomingMessage, ServerResponse } from "http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
+// Extendemos el tipo nativo de Node para que TypeScript reconozca el 'id' que inyecta pino
+interface PinoRequest extends IncomingMessage {
+  id?: string | number;
+}
+
 const app: Express = express();
 
+// @ts-expect-error - Ignoramos el falso positivo de tipo callable por el conflicto ESM/CJS en Vercel
 app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: PinoRequest) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: ServerResponse) {
         return {
           statusCode: res.statusCode,
         };
       },
     },
-  }),
+  })
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
